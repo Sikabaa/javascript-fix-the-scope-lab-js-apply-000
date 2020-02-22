92542bd74f5766bf3e6816b342bdfc0cba402257
Mercy Atiemo <mercyatiemo@gmail.com>
2020-02-22 02:52:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var animal = 'dog'
 
 function myAnimal() {return animal} 
 
-function yourAnimal() {var animal = 'cat' return animal}
+function yourAnimal() {
+  var animal = 'cat' 
+  return animal}
 
 function add2(n) {
   return n + two
